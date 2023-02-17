d590f94e1f8037f86def13e398aec6c46888cc39
Maximiliano Danna <maximilianodanna@hotmail.com>
2023-02-17 09:26:28 +0100

    hero intento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Directive1Directive } from './directives/directive1.directive';
+import { Pipe1Pipe } from './pipes/pipe1.pipe';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Directive1Directive,
+    Pipe1Pipe,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
